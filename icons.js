e76553280c171d87afa8f57e38c76e504391d790
Arturo Torres <dvluistorres@gmail.com>
2023-01-24 13:41:54 -0500

    Add js json to file

---
+++ icons.js
@@ -0,0 +1,13 @@
+const icons = {
+    darkCloud : "fa-solid fa-cloud",
+    bolt : "fa-solid fa-cloud-bolt",
+    sun : "fa-solid fa-sun",
+    wind : "fa-solid fa-wind",
+    tornado : "fa-solid fa-tornado",
+    temperature : "fa-solid fa-temperature-half",
+    smog : "fa-solid fa-smog",
+    snow : "fa-solid fa-snowflake",
+    rain : "fa-solid fa-cloud-showers-heavy",
+    sunRain : "fa-solid fa-cloud-sun-rain",
+    sunCloud : "fa-solid fa-cloud-sun",
+}
--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const APIkey = "5290321cd72fde7bc229af09c5895949"
 
+const icons = {
+  darkCloud : "fa-solid fa-cloud",
+  bolt : "fa-solid fa-cloud-bolt",
+  sun : "fa-solid fa-sun",
+  wind : "fa-solid fa-wind",
+  tornado : "fa-solid fa-tornado",
+  temperature : "fa-solid fa-temperature-half",
+  smog : "fa-solid fa-smog",
+  snow : "fa-solid fa-snowflake",
+  rain : "fa-solid fa-cloud-showers-heavy",
+  sunRain : "fa-solid fa-cloud-sun-rain",
+  sunCloud : "fa-solid fa-cloud-sun",
+}
+
 async function getUserLocation(){
   
   const successCallback = (position) => {
@@ -36,6 +50,8 @@ async function getWeatherData(request, mode = "city"){
   });
   const weatherData = await fetchData.json();
   console.log(weatherData);
+  setDataDom(weatherData);
+
 }
 
 const searchButton = document.getElementById('search');
@@ -43,4 +59,22 @@ searchButton.addEventListener('click',() => {
   const cityName = document.getElementById('cityName').value;
   getWeatherData(cityName);
 })
+
+function setDataDom(weatherData){
+  const cityData =  {
+  city : weatherData.name,
+  climate : weatherData.weather[0].description,
+  date : new Date (weatherData.dt * 1000).toLocaleDateString("default"),
+  hour : new Date (weatherData.dt * 1000).toLocaleTimeString("default"),
+  temperature : weatherData.main.temp,
+  tempFeeling : weatherData,
+  humidity : weatherData.main.humidity,
+  rainChance : weatherData,
+  windSpeed : weatherData,
+  futureDaily : {},
+  futureHourly : {}
+  }
+  console.log(cityData);
+}
+
 getUserLocation();
